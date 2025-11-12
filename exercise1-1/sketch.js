a74bc41eb7ab2c42ca0231c2c1bfc5c79e030cb9
Aha2002-Tech <andeifhassan@gmail.com>
2025-11-12 10:41:18 +0000

    ..................(:

--- exercise1-1/sketch.js
+++ exercise1-1/sketch.js
@@ -1,33 +1,30 @@
 let modeButton;
-let buttonText = "add circle";
 let circles = [];
 
 function setup() {
   createCanvas(400, 400);
   noStroke();
-  buttonSetup();
-}
-
-function buttonSetup() {
-  modeButton = createButton(buttonText);
-  modeButton.size(100, 40);
-  modeButton.position(width / 2 - 50, height - 50);
+  modeButton = createButton("add circle");
+  const container = select("main");
+  modeButton.parent(container);
+  modeButton.size(400, 80);
+  modeButton.position(0, height - 80);
   modeButton.mouseClicked(addCircle);
 }
 
 function draw() {
-  background(220);
+  background(0,255,255);
 
   for (let c of circles) {
     fill(c.col);
-    ellipse(c.x, c.y, c.r);
+    ellipse(c.x, c.y, c.r, c.r); 
   }
 }
 
 function addCircle() {
   circles.push({
     x: random(width),
-    y: random(height - 60),
+    y: random(height - 80), 
     r: random(20, 60),
     col: color(random(255), random(255), random(255))
   });
